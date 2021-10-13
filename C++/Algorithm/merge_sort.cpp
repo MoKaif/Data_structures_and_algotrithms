d6fa2c6e6d2d8e426f92dd84bb08f6dbf79189aa
MoKaif <kaif44jr@gmail.com>
2021-10-13 18:54:14 +0530

    Merge Sort code

--- C++/Algorithm/merge_sort.cpp
+++ C++/Algorithm/merge_sort.cpp
@@ -1,63 +1,11 @@
 #include <iostream>
 using namespace std;
 
-void merge(int arr[], int l, int m, int r, int size)
+void quickSort(int arr[], int s, int e)
 {
-    int i = l;
-    int j = m + 1;
-    int k = l;
-
-    /* create temp array */
-    int temp[size];
-
-    while (i <= m && j <= r) {
-        if (arr[i] <= arr[j]) {
-            temp[k] = arr[i];
-            i++;
-            k++;
-        }
-        else {
-            temp[k] = arr[j];
-            j++;
-            k++;
-        }
-    }
-    /* Copy the remaining elements of first half, if there are any */
-    while (i <= m) {
-        temp[k] = arr[i];
-        i++;
-        k++;
-    }
-
-    /* Copy the remaining elements of second half, if there are any */
-    while (j <= r) {
-        temp[k] = arr[j];
-        j++;
-        k++;
-    }
-
-    /* Copy the temp array to original array */
-    for (int p = l; p <= r; p++) {
-        arr[p] = temp[p];
-    }
-}
-
-/* l is for left index and r is
-right index of the
-sub-array of arr to be sorted */
-void mergeSort(int arr[], int l, int r, int size)
-{
-    if (l < r) {
-        // find midpoint
-        int m = (l + r) / 2;
-
-        /* recurcive mergesort first
-  and second halves */
-        mergeSort(arr, l, m, size);
-        mergeSort(arr, m + 1, r, size);
-
-        // merge
-        merge(arr, l, m, r, size);
+    if(s<e)
+    {
+        int p = partition(int arr[])
     }
 }
 
@@ -77,7 +25,7 @@ int main()
         cout << myarray[i] << " ";
     }
     cout << endl;
-    mergeSort(myarray, 0, (size - 1), size); // mergesort(arr,left,right) called
+    quickSort(myarray, 0, (size - 1), size); //
 
     cout << "After Sorting" << endl;
     for (int i = 0; i < size; i++) {
